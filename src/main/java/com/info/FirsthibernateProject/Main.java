008a7e7bec4d5dc0c5c5c1101bd97adf787922e5
Kunal Malhotra <kunalmalhotra9322@gmail.com>
2017-09-08 13:24:11 +0530

    Added hql select query in main class

--- src/main/java/com/info/FirsthibernateProject/Main.java
+++ src/main/java/com/info/FirsthibernateProject/Main.java
@@ -1,5 +1,7 @@
 package com.info.FirsthibernateProject;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -8,21 +10,29 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Write the Student_Info object into the database
+		System.out.println("Inserting data into the database");
 		Student_Info student = new Student_Info();
-
-		student.setName("Gargi");
-		student.setRoll_no(5);
-
+		student.setName("Rishi");
+		student.setRoll_no(9);
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-
 		// this would save the Student_Info object into the database
 		session.save(student);
-		System.out.println("Im here main 4");
 		session.getTransaction().commit();
 		session.close();
+
+		System.out.println("Fetching data from the database");
+		Session session1 = sessionFactory.openSession();
+		// In session.createQuery("Pass the className instead of Database Table
+		// Name")
+		List<Student_Info> studentInfoList = session1.createQuery("FROM Student_Info").list();
+		for (Student_Info eachStudentInfo : studentInfoList) {
+			Student_Info st = (Student_Info) eachStudentInfo;
+			System.out.println(st.getName() + " " + st.getRoll_no());
+		}
+		session1.close();
+
 		sessionFactory.close();
 	}
 }
